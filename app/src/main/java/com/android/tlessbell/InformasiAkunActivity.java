bb4376a8f045f02380050b320fe6d87d438e633a
nisrinaqa28 <nisrina.qurratuain.tif19@polban.ac.id>
2022-04-15 22:44:20 +0700

    UI akun, informasi akun, ubah nama, dan ubah id alat udah done

---
+++ app/src/main/java/com/android/tlessbell/InformasiAkunActivity.java
@@ -0,0 +1,13 @@
+package com.android.tlessbell;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InformasiAkunActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informasi_akun);
+    }
+}
---
+++ app/src/main/java/com/android/tlessbell/UbahIdAlatActivity.java
@@ -0,0 +1,14 @@
+package com.android.tlessbell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UbahIdAlatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_id_alat);
+    }
+}
