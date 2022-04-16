e8606d589981e9838a83f0d34a622e77b4306174
nisrinaqa28 <nisrina.qurratuain.tif19@polban.ac.id>
2022-04-16 23:36:00 +0700

    UI kalimat pesan dan ubah password done

---
+++ app/src/main/java/com/android/tlessbell/KonfirmasiUbahPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.android.tlessbell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KonfirmasiUbahPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konfirmasi_ubah_password);
+    }
+}
---
+++ app/src/main/java/com/android/tlessbell/PengaturanKalimatPesanActivity.java
@@ -0,0 +1,14 @@
+package com.android.tlessbell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PengaturanKalimatPesanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengaturan_kalimat_pesan);
+    }
+}
